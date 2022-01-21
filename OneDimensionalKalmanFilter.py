417d2ebd5a1350c48222d3ad8e703b49a88edab7
cisimon7 <cisimon7@gmail.com>
2022-01-21 22:52:53 +0300

    Refactoring

---
+++ OneDimensionalKalmanFilter.py
@@ -0,0 +1,67 @@
+import numpy as np
+import plotly.graph_objects as go
+from typing import Callable, List, Tuple, Dict
+
+
+class OneDimensionalKalmanFilter:
+    def __init__(self, system_dynamic_model_func, variance_extrapolation_func, init_state, init_variance):
+        # Initialization step
+        self.K_gain = []  # Holds current kalman filter gain
+        self.prev_estimate = init_state
+        self.prev_variance = [init_variance]
+        self.measurement_variance = [init_variance]
+        self.sys_model = system_dynamic_model_func
+        self.var_extrapolate = variance_extrapolation_func
+        self.n = init_state.flatten().shape[0]
+
+    def predict(self):
+        # predict next state using state space model
+        next_state_pred = self.sys_model(self.prev_estimate)
+
+        # predict state estimate uncertainty
+        var_extrap = self.var_extrapolate(self.prev_variance[-1])
+
+        return next_state_pred, var_extrap
+
+    def update(self, measurement, measurement_uncertainty, state_prediction, variance_extrapolation):
+        # update Kalman gain
+        k_gain = variance_extrapolation / (variance_extrapolation + measurement_uncertainty)
+        self.K_gain.append(k_gain)
+
+        # update state estimate
+        self.prev_estimate = ((np.eye(self.n) - k_gain) @ state_prediction) + (k_gain @ measurement)
+
+        # update state estimate uncertainty
+        variance = (np.eye(self.n) - k_gain) @ self.prev_variance[-1]
+        self.prev_variance.append(variance)
+
+    def next_state_estimate(self, measurement: np.ndarray, measurement_uncertainty: np.ndarray):
+        self.measurement_variance.append(measurement_uncertainty)
+
+        # loop to run steps while new measurement exist
+        (state_pred, var_extrap) = self.predict()
+        self.update(measurement, measurement_uncertainty, state_pred, var_extrap)
+
+        return self.prev_estimate, self.prev_variance[-1]
+
+    def plot_kalman_gain(self):
+        gains = np.asarray(self.K_gain).flatten()
+        count = np.asarray([i for i in np.arange(start=0, stop=len(self.K_gain), step=1)])
+        go.Figure(
+            data=[go.Scatter(x=count, y=gains, name="True Value")],
+            layout=go.Layout(title="Kalman Gain", yaxis=dict(title="Gain"), xaxis=dict(title="Measurement number"))
+        ).show()
+
+    def plot_estimate_uncertainty(self):
+        measurement_uncertainty = np.sqrt(np.asarray(self.prev_variance[1:]).flatten())
+        estimate_uncertainty = np.sqrt(np.asarray(self.measurement_variance[1:]).flatten())
+        count = np.asarray([i for i in np.arange(start=0, stop=len(self.K_gain), step=1)])
+        go.Figure(
+            data=[
+                go.Scatter(x=count, y=measurement_uncertainty, name="Measurement Uncertainty"),
+                go.Scatter(x=count, y=estimate_uncertainty, name="Estimate Uncertainty")
+            ],
+            layout=go.Layout(title="Kalman Gain",
+                             yaxis=dict(title="uncertainty"),
+                             xaxis=dict(title="Measurement number"))
+        ).show()
--- examples/KalmanFilter/BuildingHeight.py
+++ examples/KalmanFilter/BuildingHeight.py
@@ -1,14 +1,23 @@
 import numpy as np
 import plotly.graph_objects as go
 
-from KalmanFilter import KalmanFilter
+from OneDimensionalKalmanFilter import OneDimensionalKalmanFilter
 
 if __name__ == "__main__":
+    """
+    ESTIMATING THE HEIGHT OF A BUILDING
+    Building height is constant with true height being 50 meters
+    The altimeter measurement error (standard deviation) is 5 meters.
+    Initial estimate is given by human approximation as 60  meters and human estimation error taken as 15 meters
+    """
+
     measurement_error = 5
+
+    # set of 10 measurements
     measurements = np.array([48.54, 47.11, 55.01, 55.15, 49.89, 40.85, 46.72, 50.05, 51.27, 49.95])
     actual_height = np.asarray([50 for _ in measurements])  # Actual height is always constant
 
-    kalman_filter = KalmanFilter(
+    kalman_filter = OneDimensionalKalmanFilter(
         system_dynamic_model_func=(lambda x: x),
         variance_extrapolation_func=(lambda x: x),
         init_state=np.array([60]),
@@ -18,7 +27,7 @@ if __name__ == "__main__":
     estimates = np.asarray([
         kalman_filter.next_state_estimate(
             measurement=np.array([measure]),
-            measurement_uncertainty=np.array([25])
+            measurement_uncertainty=np.array([measurement_error ** 2])
         )[0] for measure in measurements
     ])
 
@@ -28,5 +37,13 @@ if __name__ == "__main__":
             go.Scatter(x=x_axis, y=actual_height, name="True Value"),
             go.Scatter(x=x_axis, y=measurements, name="Measurements"),
             go.Scatter(x=x_axis, y=estimates.flatten(), name="Estimate"),
-        ]
+        ],
+        layout=go.Layout(
+            title="Building Height Estimation",
+            yaxis=dict(title="Height (meters)"),
+            xaxis=dict(title="Measurement number")
+        )
     ).show()
+
+    kalman_filter.plot_kalman_gain()
+    kalman_filter.plot_estimate_uncertainty()
