7fa813c72edea43b812bea1667679d38db8eae96
cisimon7 <cisimon7@gmail.com>
2022-03-28 17:15:08 +0300

    test case 4 for LCSE

--- tests/test_LinearConstraintStateSpaceModel.py
+++ tests/test_LinearConstraintStateSpaceModel.py
@@ -74,34 +74,70 @@ class TestLinearConstraintStateSpaceModel(TestCase):
         system.ode_gain_solve(time_space=np.linspace(0, 20, int(2E3)))
         system.plot_states()
 
+    def test_system4(self):
+        system = LinearConstraintStateSpaceModel(
+            A=np.array([
+                [0.79898356, 0.93832862, 0.30943309, 0.9329607, 0.52567468],
+                [0.43480883, 0.77582657, 0.71322907, 0.56725567, 0.02921253],
+                [0.07447839, 0.74912897, 0.12599992, 0.27272414, 0.33534004],
+                [0.30690427, 0.0917924, 0.29935492, 0.93765648, 0.56331568],
+                [0.74560995, 0.43528731, 0.32654366, 0.00334108, 0.83872725]
+            ]),
+            B=np.array([
+                [0.53678534, 0.93797791, 0.37248788],
+                [0.50745662, 0.23616261, 0.58877284],
+                [0.97512747, 0.32282102, 0.7786647],
+                [0.54521594, 0.63116388, 0.27805539],
+                [0.99799447, 0.72076249, 0.06001138]
+            ]),
+            G=np.array([
+                [0.472, 0.19, 0.293, 0.351, 0.396],
+                [0.627, 0.586, 0.134, 0.558, 0.203],
+                [0.298, 0.282, 0.66, 0.604, 0.067],
+                [10.878, 8.182, 8.036, 11.5, 5.261],
+                [4.601, 3.32, 5.926, 6.19, 2.256]
+            ]),
+            F=np.array([
+                [0.42111223, 0.08285465, 0.31751467, 0.50741614, 0.78448954, 0.39717398],
+                [0.1612954, 0.89717344, 0.7428686, 0.14443119, 0.6704288, 0.03088425],
+                [0.03849139, 0.73873068, 0.79369482, 0.13195193, 0.15142974, 0.61221381],
+                [0.07526989, 0.12390503, 0.62527005, 0.36274025, 0.99327459, 0.25669884],
+                [0.82678452, 0.27598196, 0.43322599, 0.59732486, 0.80116294, 0.01101946]
+            ]),
+            init_state=np.array([0.56375954, 0.18230589, 0.46795963, 0.79780289, 0.0667068])
+        )
+        system.ode_gain_solve(time_space=np.linspace(0, 20, int(2E3)))
+        system.plot_states()
+
     def test_random_system(self):
-        n = 4
-        u = 3
-        A = np.random.randn(n, n)
-        B = np.random.randn(n, u)
-        F = np.random.randn(n, 6)
-        state = np.random.randn(n)
+        n = 4  # size of state vector
+        u = 3  # size of controller vector
+        k = n  # number of constraints
+        A = np.random.rand(n, n)
+        B = np.random.rand(n, u)
+        F = np.random.rand(n, 6)
+        state = np.random.rand(n)
 
         # Generate random row space basis
-        row_space = 10 * np.random.random_sample() * np.random.randn(np.random.choice(np.arange(1, n)), n)
-        assert matrix_rank(row_space) == len(row_space)  # Make sure rows are independent
+        row_space = 10 * np.round(np.random.random_sample() * np.random.rand(np.random.choice(np.arange(1, k)), n), 4)
+
+        # Make sure rows are independent
+        assert matrix_rank(row_space) == len(row_space), f"row_space vector:\n{row_space}\nnot independent, try again"
 
         part = np.asarray([
-            np.sum(np.asarray([np.random.random_sample() * vector for vector in row_space]), axis=0)
-            for _ in range(n - len(row_space))
+            np.sum(np.asarray([np.random.randint(0, 10) * vector for vector in row_space]), axis=0)
+            for _ in range(k - len(row_space))
         ])  # Generate random linear combination of row space of length n - len(row_space)
 
         G = np.block([[row_space], [part]])  # Form Constraint matrix from row space
         R, _, _, N = subspaces_from_svd(G)
-        print(f"G:\n{G}\n")
-        print(f"{N}\n")
-        print(f"{R}\n")
 
         system = LinearConstraintStateSpaceModel(A=A, B=B, G=G, F=F, init_state=state)
-        system.ode_gain_solve(time_space=np.linspace(0, 10, int(2E3)))
+        system.ode_gain_solve(time_space=np.linspace(0, 20, int(2E3)))
         system.plot_states()
 
         print(f"A:\n{A}\n")
         print(f"B:\n{B}\n")
+        print(f"G:\n{G}\n")
         print(f"F:\n{F}\n")
         print(f"initial state:\n{state}\n")
